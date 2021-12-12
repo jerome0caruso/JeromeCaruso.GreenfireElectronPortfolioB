b5c9c7f9b5fa778af851f116a111d6acbde8af16
Jerome <jerome0caruso@gmail.com>
2021-12-12 12:31:19 -0600

    formatted code

--- server.js
+++ server.js
@@ -1,45 +1,41 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const QRCode = require('easyqrcodejs-nodejs');
+const cors = require("cors");
+const QRCode = require("easyqrcodejs-nodejs");
 app.use(cors());
 app.use(
-    express.urlencoded({
-      extended: true
-    })
-  );
+  express.urlencoded({
+    extended: true,
+  })
+);
 app.use(express.json());
 
-app.post('/',(req, res) => {
-    
-   const currencyQrCodes = req.body.map(async(currency) => {
-       
-        const qrCode = await createQR(currency);
-        const enhanchedCurrency = {...currency, qrCode};
-        console.log(enhanchedCurrency)
-        return enhanchedCurrency;
-    })
-    Promise.all(currencyQrCodes).then(data => {
-        res.send(data);
-    });
+app.post("/", (req, res) => {
+  const currencyQrCodes = req.body.map(async (currency) => {
+    const qrCode = await createQR(currency);
+    const enhanchedCurrency = { ...currency, qrCode };
+    console.log('update: updating prices');
+    return enhanchedCurrency;
+  });
+  Promise.all(currencyQrCodes).then((data) => {
+    res.send(data);
+  });
 });
 
-
 async function createQR(currency) {
-        const options = {
-            text: `${currency.name}: ${currency.price || "Crypto Project"}`,
-            width: 100,
-            height: 100,
-            logo: currency.logo, 
-		    logoWidth: 30, // fixed logo width. default is `width/3.5`
-		    logoHeight: 30, // fixed logo height. default is `heigth/3.5`
-        };
-        const qrcode = new QRCode(options);
-        return await qrcode.toDataURL();
-        // qrcode.saveImage({
-        // path: `C:/Users/Admin/Desktop/${cryptoNames[index]}.png` // save path
-        //  });
+  const options = {
+    text: `${currency.name}: ${currency.price || "Crypto Project"}`,
+    width: 100,
+    height: 100,
+    logo: currency.logo,
+    logoWidth: 30, // fixed logo width. default is `width/3.5`
+    logoHeight: 30, // fixed logo height. default is `heigth/3.5`
+  };
+  const qrcode = new QRCode(options);
+  return await qrcode.toDataURL();
+  // qrcode.saveImage({
+  // path: `C:/Users/Admin/Desktop/${cryptoNames[index]}.png` // save path
+  //  });
 }
 
 app.listen(5000);
-
